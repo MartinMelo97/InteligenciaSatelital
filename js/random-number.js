959ade7d8c2c59e08b90af1a4e310b6a65588322
TecMartinMelo <nitram-210397@hotmail.com>
2016-07-30 13:47:56 -0500

    Arreglamos funcionalidad de número aleatorio y comparación al momento de enviar formulario con JS

---
+++ js/random-number.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+	var number = Math.floor(Math.random()* 9000) + 1000;
+	document.getElementById("number").value = number;
+
+
+});
+
+function CompareNumbers() {
+
+	var original = document.getElementById('number').value;
+	var inputted = document.getElementById('code').value;
+
+	if(original == inputted)
+	{
+		alert('Los numeros coinciden!');
+	}
+	else
+	{
+		alert('Los números no coinciden, inténtalo nuevamente')
+		var number = Math.floor(Math.random()* 9000) + 1000;
+		document.getElementById("number").value = number;
+		document.getElementById("code").value = null;
+	}
+
+}
